fdc6309748d41fbb4f300a19f3b51aa3851fb261
Vlalin <vlalin.programming@gmail.com>
2019-10-20 17:34:21 +0300

    Был реализован шаблон текста с видео

--- Bot_UI_New/UserControlTextVideo.xaml.cs
+++ Bot_UI_New/UserControlTextVideo.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Twilio.TwiML.Messaging;
+
 
 namespace Bot_UI_New
 {
@@ -20,9 +23,41 @@ namespace Bot_UI_New
     /// </summary>
     public partial class UserControlTextVideo : UserControl
     {
+        
+
         public UserControlTextVideo()
         {
             InitializeComponent();
+        }
+
+        private void OpenVideo(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog video = new OpenFileDialog();
+
+            video.Filter = "Image Files(*.AVI;*.MKV;*.MOV;*.FLV;*.VOB;*.MP4;)|*.AVI;*.MKV;*.MOV;*.FLV;*.VOB;*.MP4;";
+
+            if (video.ShowDialog() == true)
+            {
+                MediaElement.Source = new Uri(video.FileName);
+                MediaElement.Pause();
+            }
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement.Play();
+        }
+
+        private void Pause_Click(object sender, RoutedEventArgs e)
+        {
+            if (MediaElement.CanPause)
+                MediaElement.Pause();
+            
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement.Stop();
         }
 
         
